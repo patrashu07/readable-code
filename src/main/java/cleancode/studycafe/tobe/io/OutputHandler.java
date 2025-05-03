9547f907f302d4776458e72444f5a76d9972f327
ksjeong <ksjeong@ntels.com>
2025-05-03 17:26:14 +0900

    [리팩토링] : 추상화 레벨 - 중복 소스 제거

--- src/main/java/cleancode/studycafe/tobe/io/OutputHandler.java
+++ src/main/java/cleancode/studycafe/tobe/io/OutputHandler.java
@@ -4,6 +4,7 @@ import cleancode.studycafe.tobe.model.StudyCafeLockerPass;
 import cleancode.studycafe.tobe.model.StudyCafePass;
 
 import java.util.List;
+import java.util.Optional;
 
 public class OutputHandler {
 
@@ -41,6 +42,12 @@ public class OutputHandler {
         System.out.println(askMessage);
         System.out.println("1. 예 | 2. 아니오");
     }
+
+  public void showPassOrderSummary(StudyCafePass selectedPass) {
+      showPassOrderSummary(selectedPass , null);
+
+  }
+
 
     public void showPassOrderSummary(StudyCafePass selectedPass, StudyCafeLockerPass lockerPass) {
         System.out.println();
